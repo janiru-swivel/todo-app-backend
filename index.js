9937943046b5fb97c1ff19394a275d263580d43f
janiru-swivel <janiru@swivelgroup.com.au>
2025-02-09 20:16:05 +0530

    swagger-api-documentation implementation (working)

--- index.js
+++ index.js
@@ -2,25 +2,45 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-import route from "./routes/todoRoute.js";
 import cors from "cors";
+import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
+import route from "./routes/todoRoute.js";
 
 const app = express();
-app.use(bodyParser.json());
-app.use(cors());
+
+// Configure environment variables
 dotenv.config();
 
+// Load Swagger document
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Swagger UI setup
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Environment variables
 const PORT = process.env.PORT || 7000;
 const MONGOURL = process.env.MONGO_URL;
 
+// Routes
+app.use("/api", route);
+
+// MongoDB connection and server startup
 mongoose
   .connect(MONGOURL)
   .then(() => {
     console.log("DB connected successfully.");
     app.listen(PORT, () => {
       console.log(`Server is running on port: ${PORT}`);
+      console.log(
+        `API Documentation available at http://localhost:${PORT}/api-docs`
+      );
     });
   })
   .catch((error) => console.log(error));
 
-app.use("/api", route);
+export default app;
--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -10,11 +10,52 @@ import {
 
 const route = express.Router();
 
+/**
+ * @swagger
+ * /todo:
+ *   post:
+ *     summary: Create a new todo
+ */
 route.post("/todo", createTodo);
+
+/**
+ * @swagger
+ * /todos:
+ *   get:
+ *     summary: Get all todos
+ */
 route.get("/todos", getAllTodos);
+
+/**
+ * @swagger
+ * /todo/{id}:
+ *   get:
+ *     summary: Get a todo by ID
+ */
 route.get("/todo/:id", getTodoById);
+
+/**
+ * @swagger
+ * /todo/{id}:
+ *   put:
+ *     summary: Update a todo
+ */
 route.put("/todo/:id", updateTodo);
+
+/**
+ * @swagger
+ * /todo/toggle/{id}:
+ *   patch:
+ *     summary: Toggle todo completion status
+ */
 route.patch("/todo/toggle/:id", toggleTodoStatus);
+
+/**
+ * @swagger
+ * /todo/{id}:
+ *   delete:
+ *     summary: Delete a todo
+ */
 route.delete("/todo/:id", deleteTodo);
 
 export default route;
